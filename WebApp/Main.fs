acacf70a7ed5390fd86ea1ea5c68f43ee2e2b9fd
Rung <rungwiroon@gmail.com>
2021-03-02 18:41:52 +0700

    Add create item page

--- WebApp/Main.fs
+++ WebApp/Main.fs
@@ -4,6 +4,8 @@ open Elmish
 open Bolero
 open Bolero.Html
 open System.Text.Json
+open System.Net.Http
+open System.Text
 
 type Page =
     | Overview
@@ -12,13 +14,15 @@ type Page =
 type Model =
     {
         Page: Page
-        AddItem: NewItem option
+        NewItem: NewItem option
         Overview: InventoryOverview option
         Error: string option
     }
 and NewItem =
     {
+        Id: string option
         Name: string
+
     }
 and InventoryItem =
     {
@@ -37,13 +41,13 @@ type InventoryService =
         GetOverview: unit -> Async<InventoryOverview>
 
         /// Add a item in the collection.
-        AddItem: NewItem -> Async<unit>
+        AddItem: NewItem -> Async<string>
     }
 
 let initModel =
     {
         Page = Overview
-        AddItem = None
+        NewItem = None
         Overview = None
         Error = None
     }
@@ -52,10 +56,12 @@ type Message =
     | SetPage of Page
     | GetOverview
     | ShowOverview of InventoryOverview
-    | AddItem of NewItem
     | Error of exn
+    | SetNameForCreate of string
+    | CreateItemCmd
+    | ItemCreated of string
 
-let httpClient = new System.Net.Http.HttpClient()
+let httpClient = new HttpClient()
 
 let inventoryService : InventoryService =
     {
@@ -69,7 +75,14 @@ let inventoryService : InventoryService =
             }
         AddItem = fun (newItem : NewItem) ->
             async {
-                return ()
+                let url = "https://localhost:5001/inventory/"
+                let json = JsonSerializer.Serialize(newItem)
+                let reqContent = new StringContent(json, Encoding.UTF8, "application/json")
+                let! response = httpClient.PostAsync(url, reqContent) |> Async.AwaitTask
+                response.EnsureSuccessStatusCode () |> ignore
+                let! respContent = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+                return respContent;
+                //return! JsonSerializer.DeserializeAsync<InventoryOverview>(content).AsTask() |> Async.AwaitTask
             }
     }
 
@@ -85,9 +98,17 @@ let update message model =
         Cmd.OfAsync.either inventoryService.GetOverview () ShowOverview Error
     | ShowOverview overview ->
         { model with Overview = Some overview }, Cmd.none
-    | AddItem newItem ->
-        (inventoryService.AddItem newItem) |> Async.RunSynchronously
-        { model with AddItem = Some newItem }, Cmd.none
+    | SetNameForCreate itemName ->
+        { model with NewItem = Some { Id = None; Name = itemName } }, Cmd.none
+    | CreateItemCmd ->
+        match model.NewItem with
+        | Some value ->
+            model,
+            Cmd.OfAsync.either inventoryService.AddItem (value) ItemCreated Error
+        | None -> model, Cmd.none
+    | ItemCreated id ->
+        let newItem = model.NewItem |> Option.map (fun x -> { x with Id = Some id })
+        { model with NewItem = newItem; Page = Overview }, Cmd.none
     | Error exn ->
         { model with Error = Some exn.Message }, Cmd.none
 
@@ -96,7 +117,7 @@ let router = Router.infer SetPage (fun m -> m.Page)
 type Overview = Template<"""
 <div id="hello">
     <p>
-        <a href="#/create" class="btn">Create Inventory Item</a>
+        <a href="/Add" class="btn">Create Inventory Item</a>
     </p>
 
     <h3>Items</h3>
@@ -122,9 +143,43 @@ let overviewPage model dispatch =
                 ])
         .Elt()
 
+type CreateItem = Template<"""
+<div class="row-fluid">
+    <h2>Add New Inventory Item</h2>
+    <form id="inventoryItemForm" class="form-horizontal" onsubmit="${Submit}">       
+        <div class="control-group">
+            <label class="control-label" for="name">Name</label>
+            <div class="controls">
+                <input id="name" name="name" type="text" placeholder="Name" bind-onchange="${Name}"/>
+            </div>
+        </div>
+        <div class="control-group">
+            <div class="controls">
+                <button class="btn btn-primary" type="submit">Save</button>
+            </div>
+        </div>
+    </form>
+</div>
+""">
+
+let createItemPage (model : Model) dispatch =
+    match model.NewItem with
+    | Some item ->
+        CreateItem()
+            .Name(item.Name, fun n -> n |> SetNameForCreate |> dispatch)
+            .Submit(fun _ -> dispatch CreateItemCmd)
+            .Elt()
+    | None ->
+        CreateItem()
+            .Name("", fun n -> n |> SetNameForCreate |> dispatch)
+            .Submit(fun _ -> dispatch CreateItemCmd)
+            .Elt()
+    
+
 let view model dispatch =
     cond model.Page <| function
     | Overview -> overviewPage model dispatch
+    | Add -> createItemPage model dispatch
     | _ -> text "Hello, world!"
 
 type MyApp() =
